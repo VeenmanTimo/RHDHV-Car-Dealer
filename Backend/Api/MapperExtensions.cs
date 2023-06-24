afe82f696b14efedb356e6c0ecedb6c0bc5b960e
Timo <veenmantimo@gmail.com>
2023-06-24 15:09:23 +0200

    Added request calls to CarService

---
+++ Backend/Api/MapperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+
+namespace Api;
+
+public static class MapperExtensions
+{
+    public static Car ToResponseCar(this CarModel model)
+    {
+        return new Car
+        {
+            Guid = model.Guid.ToString(),
+            Model = model.Model,
+            Make = model.Make,
+            Version = model.Version,
+            YearOfRelease = model.YearOfRelease,
+            Price = model.Price,
+            FuelComsumption = model.FuelConsumption,
+            AnnualMaintenanceCost = model.AnnualMaintenanceCost,
+        };
+    }
+}
--- Backend/Api/Services/CarService.cs
+++ Backend/Api/Services/CarService.cs
@@ -1,21 +1,56 @@
 ﻿using Grpc.Core;
+using MediatR;
 
 namespace Api.Services;
 
 public class CarService : CarEndpoints.CarEndpointsBase
 {
-    public override Task<CreateCarResponse> CreateCar(CreateCarRequest request, ServerCallContext context)
+    public CarService(IMediator mediator)
     {
-        return Task.FromResult(new CreateCarResponse { Car = new()});
+        this.mediator = mediator;
     }
 
-    public override Task<GetAllResponse> GetAll(GetAllRequest request, ServerCallContext context)
+    private readonly IMediator mediator;
+
+    public override async Task<CreateCarResponse> CreateCar(CreateCarRequest car, ServerCallContext context)
     {
-        return Task.FromResult(new GetAllResponse());
+        var result = await mediator.Send(new Application.CarService.Create.CreateRequest
+        {
+            Model = car.Model,
+            Make = car.Make,
+            Version = car.Version,
+            YearOfRelease = car.YearOfRelease,
+            Price = car.Price,
+            FuelConsumption = car.FuelComsumption,
+            AnnualMaintenanceCost = car.AnnualMaintenanceCost
+        });
+
+        return new CreateCarResponse { Car = result.ToResponseCar() };
     }
 
-    public override Task<GetByGuidReponse> GetByGuid(GetByGuidRequest request, ServerCallContext context)
+    public override async Task<GetAllResponse> GetAll(GetAllRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new GetByGuidReponse());
+        var result = await mediator.Send(new Application.CarService.GetAll.GetAllRequest());
+
+        var response = new GetAllResponse();
+        response.Cars.AddRange(result.Select(item => item.ToResponseCar()));
+        return response;
+    }
+
+    public override async Task<GetByGuidReponse> GetByGuid(GetByGuidRequest request, ServerCallContext context)
+    {
+        if (!Guid.TryParse(request.Guid, out var guid))
+        {
+            return new GetByGuidReponse();
+        }
+
+        var result = await mediator.Send(new Application.CarService.GetByGuid.GetByGuidRequest { Guid = guid });
+
+        if (result == null)
+        {
+            return new GetByGuidReponse();
+        }
+
+        return new GetByGuidReponse { Car = result.ToResponseCar() };
     }
 }
