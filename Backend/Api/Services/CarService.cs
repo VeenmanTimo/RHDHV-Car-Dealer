d5755df4c85493a094b30ba767f20200bd601f48
Timo <veenmantimo@gmail.com>
2023-06-24 09:12:46 +0200

    Created backend project

---
+++ Backend/Api/Services/CarService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace Api.Services;
+
+public class CarService : CarEndpoints.CarEndpointsBase
+{
+    public override Task<CreateCarResponse> CreateCar(CreateCarRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new CreateCarResponse { Car = new()});
+    }
+
+    public override Task<GetAllResponse> GetAll(GetAllRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new GetAllResponse());
+    }
+
+    public override Task<GetByGuidReponse> GetByGuid(GetByGuidRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new GetByGuidReponse());
+    }
+}
